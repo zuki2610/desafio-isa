f41f187fae4b6bb9c075e3c3287e2b3f6f3685cb
Isabel Palacios <issabelpalacios@gmail.com>
2023-03-27 23:59:06 -0300

    ADD: redireccion para ir al home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,39 @@
-import React from "react";  
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Image } from 'react-bootstrap';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Image } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const redireccionHome = () => {
+    navigate("/desafio-isa/home");
+  };
 
-
-return (
+  return (
     <>
-    <div className="header">
+      <div className="header">
         <div className="d-flex flex-row">
-            <div className="containerHeader d-flex">
-                <Image className="logoMobile" src={process.env.PUBLIC_URL + '/carolineSada.jpg'} />
-                <div className="column text-center texttitle">
-                <span>Emprende con:</span>
-                <h2>Caroline Sada</h2>
-                </div>
-                <div> 
-                <span className="carritoLleno hover5">0<i className="bi bi-cart-check-fill"></i></span>
-
-                </div>
+          <div className="containerHeader d-flex">
+            <Image
+              onClick={() => {redireccionHome()} }
+              className="logoMobile"
+              src={process.env.PUBLIC_URL + "/carolineSada.jpg"}
+            />
+            <div className="column text-center texttitle">
+              <span>Emprende con:</span>
+              <h2>Caroline Sada</h2>
             </div>
+            <div>
+              <span className="carritoLleno hover5">
+                0<i className="bi bi-cart-check-fill"></i>
+              </span>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-
-);
-
-}
+  );
+};
 
 export default Header;
